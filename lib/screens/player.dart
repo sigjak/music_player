28e9e646d9e645b94993df99559c21366770c61b
sigjak <sigjak@gmail.com>
2021-02-28 10:52:06 +0000

    master working

--- lib/screens/player.dart
+++ lib/screens/player.dart
@@ -5,7 +5,6 @@ import 'package:flutter/services.dart';
 import 'package:just_audio/just_audio.dart';
 import './commons/player_buttons.dart';
 import './commons/slider.dart';
-import './commons/name.dart';
 
 class Player extends StatefulWidget {
   @override
@@ -15,50 +14,21 @@ class Player extends StatefulWidget {
 class _PlayerState extends State<Player> {
   AudioPlayer _audioPlayer;
   List<String> audioFiles;
+  List<AudioSource> myList = [
+    AudioSource.uri(Uri.parse("asset:///audio/book/guard.mp3")),
+  ];
   @override
   void initState() {
     super.initState();
     _audioPlayer = AudioPlayer();
-    // _audioPlayer
-    //     .setAudioSource(ConcatenatingAudioSource(children: [
-    //   AudioSource.uri(
-    //     Uri.parse("asset:///audio/book/John Grisham -TheGuardians_01.mp3,"),
-    //   ),
-    //   AudioSource.uri(
-    //     Uri.parse("asset:///audio/book/John Grisham -TheGuardians_02.mp3,"),
-    //   ),
-    //   // AudioSource.uri(
-    //   //   Uri.parse("asset:///audio/book/John Grisham -TheGuardians_03.mp3,"),
-    //   // ),
-    // ]))
-    //     .catchError((error) {
-    //   // catch load errors: 404, invalid url ...
-    //   print("An error occured $error");
-    // });
-    // Set a sequence of audio sources that will be played by the audio player.
-    _audioPlayer
-        .setAudioSource(ConcatenatingAudioSource(children: [
-      // AudioSource.uri(Uri.parse(
-      //     "https://archive.org/download/IGM-V7/IGM%20-%20Vol.%207/25%20Diablo%20-%20Tristram%20%28Blizzard%29.mp3")),
-      // AudioSource.uri(Uri.parse(
-      //     "https://archive.org/download/igm-v8_202101/IGM%20-%20Vol.%208/15%20Pokemon%20Red%20-%20Cerulean%20City%20%28Game%20Freak%29.mp3")),
-      // AudioSource.uri(Uri.parse(
-      //     "https://scummbar.com/mi2/MI1-CD/01%20-%20Opening%20Themes%20-%20Introduction.mp3")),
 
-      AudioSource.uri(Uri.parse("asset:///audio/book/guard1.mp3")),
-      AudioSource.uri(Uri.parse("asset:///audio/book/guard2.mp3")),
-      AudioSource.uri(Uri.parse("asset:///audio/book/guard3.mp3")),
-      AudioSource.uri(Uri.parse("asset:///audio/book/guard4.mp3")),
-    ]))
+    _audioPlayer
+        .setAudioSource(ConcatenatingAudioSource(children: myList))
         .catchError((error) {
       // catch load errors: 404, invalid url ...
       print("An error occured $error");
     });
   }
-
-  // initAll() async {
-  //   List<AudioSource> aa = await getAssetFiles();
-  // }
 
   @override
   void dispose() {
@@ -87,17 +57,6 @@ class _PlayerState extends State<Player> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Names(_audioPlayer),
-          ElevatedButton(
-              onPressed: () async {
-                await getAssetFiles();
-              },
-              child: Text('Get')),
-          ElevatedButton(
-              onPressed: () async {
-                await _audioPlayer.seek(Duration(milliseconds: 4000), index: 1);
-              },
-              child: Text('jump')),
           PlayerButtons(_audioPlayer),
           SliderBar(_audioPlayer),
         ],
